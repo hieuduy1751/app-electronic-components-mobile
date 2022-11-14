8eedb20494b7c2cec4ea10556d4768086849ac64
hieuduy1751 <hieuduy1751@gmail.com>
2022-11-14 13:28:19 +0700

    add: mock data

---
+++ mock-data.js
@@ -0,0 +1,14 @@
+const data = {
+  products: [
+    {
+      name: 'iPhone 14 Pro Max 128GB',
+      category: 'Phones',
+      price: 999,
+      brand: 'Apple',
+      color: ['Black', 'Silver', 'Gold'],
+      image: 'https://images.fpt.shop/unsafe/fit-in/960x640/filters:quality(90):fill(white):upscale()/fptshop.com.vn/Uploads/Originals/2022/4/19/637859769705950103_iPhone%2013%20Promax%20(1).jpg'
+    }
+  ]
+}
+
+export default data
