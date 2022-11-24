ae45e8706db4198097201ec889fb1e2bea5790a7
hieuduy1751 <hieuduy1751@gmail.com>
2022-11-24 14:39:54 +0700

    format edit address screen

--- screens/EditAddress/EditAddress.tsx
+++ screens/EditAddress/EditAddress.tsx
@@ -8,42 +8,40 @@ import {
   ActivityIndicator,
   Text,
   TouchableOpacity
-  } from 'react-native';
+} from 'react-native';
 import CInput from '../../components/CInput';
 
 export default function EditAddress() {
   return (
-    <View>
+    <SafeAreaView>
+      <View style={{ alignItems: 'center' }}>
+        <CInput placeholder='Name' customStyleContainer={{ margin: 10 }} >
+          <Image source={require('../../assets/images/Profile.png')} style={{ marginRight: 10 }} />
+        </CInput>
+        <CInput placeholder='Phone' customStyleContainer={{ margin: 10 }}>
+          <Image source={require('../../assets/images/Call.png')} style={{ marginRight: 10 }} />
 
-        <View style= {{alignItems:'center'}}>
-            <CInput placeholder='Name' customStyleContainer={{margin: 10}} >
-                <Image source={require('../../assets/images/Profile.png')} style={{marginRight:10}}/>
-            </CInput>
-            <CInput placeholder='Phone' customStyleContainer={{margin: 10}}>
-                <Image source={require('../../assets/images/Call.png')} style={{marginRight:10}}/>
-
-            </CInput>
-            <CInput placeholder='Address' customStyleContainer={{margin: 10}}>
-               <Image source={require('../../assets/images/Location.png')} style={{marginRight:10}}/>
-            </CInput>
-        </View>
-        <TouchableOpacity style={{backgroundColor:'#FB8500', alignItems:'center', margin:20, padding:20, borderRadius:20}}>
-          <Text style={styles.paymentText}>SUBMIT</Text>
-        </TouchableOpacity>
-    </View>
+        </CInput>
+        <CInput placeholder='Address' customStyleContainer={{ margin: 10 }}>
+          <Image source={require('../../assets/images/Location.png')} style={{ marginRight: 10 }} />
+        </CInput>
+      </View>
+      <TouchableOpacity style={{ backgroundColor: '#FB8500', alignItems: 'center', margin: 20, padding: 20, borderRadius: 20 }}>
+        <Text style={styles.paymentText}>SUBMIT</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
-    textSuccess:{
-        fontWeight:'bold',
-        fontSize:20,
-        alignContent:'center',
-        margin:100,
-    },
-    paymentText:{
-        color:'#ffff',
-        fontSize:20,
-        fontWeight:'bold'
-      }
+  textSuccess: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignContent: 'center',
+    margin: 100,
+  },
+  paymentText: {
+    color: '#ffff',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
-  
